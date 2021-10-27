216127695463bba831cd7771cdb675afeb820166
SamArcaro <62896598+SamArcaro@users.noreply.github.com>
2021-10-27 00:19:58 -0400

    Update ObjAngle.cs

--- DonutDunkersGame/Assets/Scripts/Level/Objects/ObjAngle.cs
+++ DonutDunkersGame/Assets/Scripts/Level/Objects/ObjAngle.cs
@@ -1,7 +1,12 @@
 using System;
+using System.Collections;
 using UnityEngine;
 
-public class ObjAngle : ObjectInteraction {
+public class ObjAngle : ObjectInteraction, ICanReset {
+	
+	public void Initialize() {
+		this.ResetRoutine();
+	}
 	
     public override void PlayerInteraction() {
 		Transform ballTransform = BallController.Instance.transform;
@@ -9,6 +14,7 @@ public class ObjAngle : ObjectInteraction {
 			BallController.Instance.IsMoving = false;
 			BallController.Instance.transform.position = this.transform.position - BallController.Instance.transform.forward;
 		} else {
+			this.DoRoutine();
 			ballTransform.position = this.transform.position;
 			if (ballTransform.forward == -this.gameObject.transform.forward) {
 				BallController.Instance.SetForwardDirection(this.gameObject.transform.up);
@@ -16,5 +22,25 @@ public class ObjAngle : ObjectInteraction {
 				BallController.Instance.SetForwardDirection(this.gameObject.transform.forward);
 			}
 		}
+	}
+	
+	private void DoRoutine() {
+		this.ResetRoutine();
+		this.hitRoutine = this.StartCoroutine(this.HitRoutine());
+	}
+	
+	private void ResetRoutine() {
+		if (this.hitRoutine != null) {
+			this.StopCoroutine(this.hitRoutine);
+			this.hitRoutine = null;
+		}
+	}
+	
+	private Coroutine hitRoutine;
+	
+	private IEnumerator HitRoutine() {
+		BallController.Instance.IsMoving = false;
+		yield return new WaitForSeconds(0.15f);
+		BallController.Instance.IsMoving = true;		
 	}
 }

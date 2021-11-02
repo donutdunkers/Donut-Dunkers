2d53eb002cfe462ccb411a1fcc8c568b042e62c7
globby342 <49796093+globby342@users.noreply.github.com>
2021-11-02 12:51:49 -0400

    Sound System Added
    Scripts that deal with storing and playing sounds

---
+++ DonutDunkersGame/Assets/Scripts/Sound/SoundEvent.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(menuName ="SoundSystem/Sound Event", fileName = "Audio_")]
+public class SoundEvent : ScriptableSingleton<SoundEvent>
+{
+    //variable for each sound effect we have
+    public Sound sndDonutget;
+
+
+
+
+
+
+    
+}

29815633785d21c3185b5ba6032a5c915a0d4e27
globby342 <49796093+globby342@users.noreply.github.com>
2021-11-02 18:00:38 -0400

    Deleted Fmod
    Deleted Fmod from the project. Audio system should be set to unity's

--- DonutDunkersGame/Assets/Scripts/Sound/SoundEvent.cs
+++ DonutDunkersGame/Assets/Scripts/Sound/SoundEvent.cs
@@ -6,8 +6,17 @@ using UnityEngine;
 [CreateAssetMenu(menuName ="SoundSystem/Sound Event", fileName = "Audio_")]
 public class SoundEvent : ScriptableSingleton<SoundEvent>
 {
-    //variable for each sound effect we have
+    //variable for each sound effect we have, one time sounds
     public Sound sndDonutget;
+    public Sound sndDonuthitWall;
+    public Sound sndDonuthitJelly;
+    public Sound sndDonuthitWater;
+    public Sound sndDonuthitFullsink;
+    public Sound sndDonuthitPlate;
+    public Sound sndDonuthitWindow;
+    public Sound sndDonutlaunch;
+
+
 
 
 

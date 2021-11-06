0dc4502da4bd761439312ee01ab859f1af65786e
globby342 <49796093+globby342@users.noreply.github.com>
2021-11-06 15:29:21 -0400

    Added Sounds and Updated Music Playing System
    added .wavs to sound scriptable object, and updated the music source controller

--- DonutDunkersGame/Assets/Scripts/Sound/SoundManager.cs
+++ DonutDunkersGame/Assets/Scripts/Sound/SoundManager.cs
@@ -16,9 +16,90 @@ public class SoundManager : MonoBehaviour
             return _Instance;
         }
     }
+    private void Update()
+    {
+        AudioSource currentSource;
+        if (isMusicSourceA)
+        {
+            currentSource = musicSourceA;
+        }
+        else
+        {
+            currentSource = musicSourceB;
+        }
+    }
+    public void CrossFade(AudioClip audio, float volume, float fadetime)
+    {
+
+    }
+    private Coroutine currentSourceroutine; //assign it to run a numerator
+    private Coroutine newSourceroutine;
+    private IEnumerator Fade(AudioClip audio, float volume, float fadetime)
+    {
+        AudioSource currentSource;
+        AudioSource newSource;
+
+        //currentSource = (isMusicSourceA) ? musicSourceA : musicSourceB;
+        if (isMusicSourceA)
+        {
+            currentSource = musicSourceA;
+        }
+        else
+        {
+            currentSource = musicSourceB;
+        }
+
+        if (currentSource.clip == audio)
+        {
+            currentSource.volume = 1f;
+            yield break;
+        }
+
+        if (isMusicSourceA)
+        {
+            newSource = musicSourceB;
+        }
+        else
+        {
+            newSource = musicSourceA;
+        }
+
+        newSource.clip = audio;
+        newSource.Play();
+        newSource.volume = 0f;
+
+        if (currentSourceroutine != null)
+        {
+            StopCoroutine(currentSourceroutine);
+        }
+        if (newSourceroutine != null)
+        {
+            StopCoroutine(newSourceroutine);
+        }
+        currentSourceroutine = StartCoroutine(FadeSource(currentSource, currentSource.volume, 0, fadetime));
+        newSourceroutine = StartCoroutine(FadeSource(newSource, newSource.volume, volume, fadetime));
+        isMusicSourceA = !isMusicSourceA;
+    }
+
+    private IEnumerator FadeSource(AudioSource source, float startV, float endV, float duration)
+    {
+        float startTime = Time.unscaledTime;
+        while (true)
+        {
+            float elapsed = Time.unscaledTime - startTime;
+            source.volume = Mathf.Clamp01(Mathf.Lerp(startV, endV, elapsed / duration));
+            if (source.volume == endV)
+            {
+                break;
+            }
+            yield return null;
+        }
+    }
 
     //plays music
-    public AudioSource musicSource;
+    public AudioSource musicSourceA;
+    public AudioSource musicSourceB;
+    private bool isMusicSourceA; 
 
     //plays sound effects
     public AudioSource soundSource;

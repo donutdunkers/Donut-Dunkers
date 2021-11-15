1d5677169252491e9fb6af92d67d5d9217a7c3ac
Tom Giagtzoglou <tom.giagtzoglou@gmail.com>
2021-11-15 14:02:28 -0500

    remove buttons from ui

--- DonutDunkersGame/Assets/Scripts/Camera/MainMenuCamera.cs
+++ DonutDunkersGame/Assets/Scripts/Camera/MainMenuCamera.cs
@@ -1,15 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Pixelplacement;
 
 public class MainMenuCamera : MonoBehaviour {
 
     public bool showingLevelSelect = false;
     public Transform mainCamPos, levelSelectCamPos;
 
+    public float transitionTime = 1f;
+
 
     void Start() {
-        this.transform.SetPositionAndRotation( mainCamPos);
+        LerpTransform(mainCamPos);
     }
 
     // Update is called once per frame
@@ -17,8 +20,19 @@ public class MainMenuCamera : MonoBehaviour {
         
     }
 
+    public void GoToLevelSelect() {
+        showingLevelSelect = true;
+        LerpTransform(levelSelectCamPos);
+    }
+
+    public void GoToMainMenu() {
+        showingLevelSelect = false;
+        LerpTransform(mainCamPos);
+    }
+
 
     void LerpTransform(Transform target) {
-        
+        Tween.Position(this.transform, target.position, transitionTime, 0, Tween.EaseInOut);
+        Tween.Rotation(this.transform, target.rotation, transitionTime, 0, Tween.EaseInOut);
     }
 }

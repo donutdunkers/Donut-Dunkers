3ec06e73c1ac6f9d910622de7b152250fdfb0e01
Tom Giagtzoglou <tom.giagtzoglou@gmail.com>
2021-11-15 19:08:22 -0500

    Basic outline donuts

--- DonutDunkersGame/Assets/Scripts/UI/SelectionDonut.cs
+++ DonutDunkersGame/Assets/Scripts/UI/SelectionDonut.cs
@@ -2,17 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Pixelplacement;
-public class SelectionDonut : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+
+[SelectionBase]
+public class SelectionDonut : MonoBehaviour {
+
+    Material[] materials;
+
+    public float baseWidth, highlightWidth, selectedWidth;
+    void Awake() {
+        materials = GetComponentInChildren<Renderer>().materials;
+
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void Update() {
+        // SetOutlineWidth(baseWidth);
+    }
+
+    void OnMouseEnter() {
+        SetOutlineWidth(highlightWidth);
+        Debug.Log("Mouse Enter");
+    }
+
+    void OnMouseExit() {
+        SetOutlineWidth(baseWidth);
+        Debug.Log("Mouse Exit");
+    }
+
+    // assumes outline is in second material slot
+    void SetOutlineWidth(float width) {
+        materials[1].SetFloat("_Amount", width);
     }
 }

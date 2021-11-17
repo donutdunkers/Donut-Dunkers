df760391acc0543cc822ebb182080446627e1465
Tom Giagtzoglou <tom.giagtzoglou@gmail.com>
2021-11-16 21:50:20 -0500

    add world and level  scriptableobjects

--- DonutDunkersGame/Assets/Scripts/Data/LevelSettings.cs
+++ DonutDunkersGame/Assets/Scripts/Data/LevelSettings.cs
@@ -2,17 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LevelSettings : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+[CreateAssetMenu(menuName = "Data/Levels/Level", fileName = "Level")]
+public class LevelSettings : ScriptableObject {
+    public string sceneName;
+    public int levelID;
 }
--- DonutDunkersGame/Assets/Scripts/Data/WorldSettings.cs
+++ DonutDunkersGame/Assets/Scripts/Data/WorldSettings.cs
@@ -2,17 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WorldSettings : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+[CreateAssetMenu(menuName = "Data/Levels/World", fileName = "World")]
+public class WorldSettings : ScriptableObject {
+    public int worldID;
+    public List<LevelSettings> levels;
+
+
 }

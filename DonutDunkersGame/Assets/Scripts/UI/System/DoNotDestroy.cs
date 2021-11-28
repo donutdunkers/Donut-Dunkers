673a5483bc8fb05a785cf22bb3b28035d2114c42
Tom Giagtzoglou <tom.giagtzoglou@gmail.com>
2021-11-28 17:40:40 -0500

    mid change broken

---
+++ DonutDunkersGame/Assets/Scripts/UI/System/DoNotDestroy.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoNotDestroy : MonoBehaviour {
+	
+    private void Awake() {
+		DontDestroyOnLoad(this.gameObject);
+	}
+}

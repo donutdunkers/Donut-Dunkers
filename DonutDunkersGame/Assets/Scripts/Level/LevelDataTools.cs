5ca47ca999e3978ff6f9de2fc89837a20e71c398
SamArcaro <62896598+SamArcaro@users.noreply.github.com>
2021-11-30 19:04:49 -0500

    Added pre-runtime wall/theme generation | Fixed model prefabs | Updated logic in ObjAngle.cs and LevelData.cs

---
+++ DonutDunkersGame/Assets/Scripts/Level/LevelDataTools.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CustomEditor(typeof(LevelDataGenerator))]
+public class LevelDataTools : Editor 
+{
+    public override void OnInspectorGUI()
+    {
+        if (GUILayout.Button("Generate Walls")) {
+			LevelData.Instance.GenerateWalls();
+		}
+		if (GUILayout.Button("Update Level Theme")) {
+			LevelData.Instance.GenerateLevelTheme();
+		}
+    }
+}

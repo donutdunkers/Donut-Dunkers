87ae138794ebf21295e6a9cb0df2ca1b277da9e1
Tom Giagtzoglou <tom.giagtzoglou@gmail.com>
2021-12-07 04:03:52 -0500

    Add stars to endgame UI

--- DonutDunkersGame/Assets/Scripts/UI/LevelEndUI.cs
+++ DonutDunkersGame/Assets/Scripts/UI/LevelEndUI.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using Pixelplacement;
 
 public class LevelEndUI : MonoBehaviour
 {
@@ -10,37 +11,65 @@ public class LevelEndUI : MonoBehaviour
     private TextMeshProUGUI titleText;
 
     [SerializeField]
-    private TextMeshProUGUI collectionText;
-
+    private TextMeshProUGUI levelNumberText;
     [SerializeField]
     private TextMeshProUGUI moveCountText;
+
+
+
+    [Header("Star Animation Settings")]
+    [SerializeField]
+    private Image[] stars;
+
+    [SerializeField]
+    private float starAnimationTime = 0.5f; 
+    [SerializeField]
+    private float delayBetweenStars = 0.15f;
+    [SerializeField]
+    private AnimationCurve easeCurve = Tween.EaseSpring;
 
     void SetTitleText(bool isOutOfTurns)
     {
         if(isOutOfTurns)
         {
             this.titleText.SetText("You Ran Out Of Moves!");
+            
         }
         else
         {
             this.titleText.SetText("Nice Job!");
         }
     }
-
-    void SetDonutsCollected(int collected, int total)
-    {
-        this.collectionText.SetText($"You collected {collected} / {total} donuts");
+    
+    void SetStars(int numStars) {
+        for(int i = 0; i < numStars; i++) {
+            // Vector2 starStarSize = new Vector2(0f, 0f);
+            Vector2 starEndSize = stars[i].rectTransform.sizeDelta;
+            stars[i].gameObject.SetActive(true);
+            stars[i].rectTransform.localScale = Vector3.zero;
+            Tween.LocalScale(stars[i].rectTransform, Vector3.zero, Vector3.one, starAnimationTime, ((i + 1) * delayBetweenStars), easeCurve, Tween.LoopType.None, null, null, false);
+        }
     }
 
-    void SetNumMoves(int value)
-    {
+    // void SetDonutsCollected(int collected, int total)
+    // {
+    //     this.collectionText.SetText($"You collected {collected} / {total} donuts");
+    // }
+
+    void SetNumMoves(int value) {
         this.moveCountText.SetText($"You took {value} moves");
     }
 
-    public void SetLevelEndUI(bool isOutOfTurns, int ringsCollected, int ringsTotal, int numMoves)
-    {
+    void SetLevelNumber(int levelNum, bool didWin) {
+        this.levelNumberText.SetText($"Level {levelNum}" + (didWin ? " Complete!" : " Failed"));
+    }
+
+    public void SetLevelEndUI(bool isOutOfTurns, int ringsCollected, int ringsTotal, int numMoves) {
+        LevelSettings currLevel = LevelInfo.Instance.currLevel;
         SetTitleText(isOutOfTurns);
-        SetDonutsCollected(ringsCollected, ringsTotal);
+        SetLevelNumber(currLevel.levelIndex + 1, !isOutOfTurns);
+        // SetDonutsCollected(ringsCollected, ringsTotal);
         SetNumMoves(numMoves);
+        SetStars(currLevel.GetNumStars(numMoves));
     }
 }
